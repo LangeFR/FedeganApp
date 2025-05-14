86ef7978c3eab1302f8738128a880ed35e5853a6
LangeFR <mdjvanegasc@gmail.com>
2025-05-14 00:57:12 -0500

    :wrench: Database config

--- fedegan/secrets.py
+++ fedegan/secrets.py
@@ -1,9 +1,7 @@
-from pydantic_settings import BaseSettings
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Secrets(BaseSettings):
     DEBUG: bool = False
-
     SECRET_KEY: str = "secret_key"
 
     DATABASE_USER: str = "postgres"
@@ -11,3 +9,5 @@ class Secrets(BaseSettings):
     DATABASE_HOST: str = "db"
     DATABASE_PORT: str = "5432"
     DATABASE_NAME: str = "fedegan-app"
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")

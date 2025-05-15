9cae1bbaa91f8799a531b911a3d6b520ae01f692
Jhojan Jimenez <jhojanjimene@gmail.com>
2025-05-14 23:19:16 -0500

    Crear Campaña y ver vacunaciones

--- campanias/urls.py
+++ campanias/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CampañaListView, VacunacionCreateView
+from .views import VacunacionListView, CampañaCreateView, CampañaListView, VacunacionCreateView
 
 urlpatterns = [
     path('campanas/', CampañaListView.as_view(), name='lista-campañas'),
     path('vacunaciones/crear/', VacunacionCreateView.as_view(),
          name='crear-vacunacion'),
+    path('campañas/crear/', CampañaCreateView.as_view(), name='crear-campaña'),
+    path('vacunaciones/', VacunacionListView.as_view(), name='lista-vacunaciones'),
 ]
--- municipio/admin.py
+++ municipio/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Municipio
 
+
 @admin.register(Municipio)
 class MunicipioAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'region')
+    list_display = ('id', 'nombre', 'region')
     list_filter = ('region',)
     search_fields = ('nombre',)
     ordering = ('nombre',)

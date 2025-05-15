04ff3c6cb87234829edeab747481f3835f501dd7
LangeFR <mdjvanegasc@gmail.com>
2025-05-14 22:53:07 -0500

    :bug: url campañas

--- campanias/urls.py
+++ campanias/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CampañaListView, VacunacionCreateView
 
 urlpatterns = [
-    path('campañas/', CampañaListView.as_view(), name='lista-campañas'),
+    path('campanas/', CampañaListView.as_view(), name='lista-campañas'),
     path('vacunaciones/crear/', VacunacionCreateView.as_view(),
          name='crear-vacunacion'),
 ]
